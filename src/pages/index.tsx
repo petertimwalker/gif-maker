930e9c9a73cdc9da1042270725fc9c48b386d8a8
Eric Lu <justswim@gmail.com>
2023-07-28 16:37:07 -0700

    add more text on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,24 @@ export default function Home() {
         <Navbar />
         <div className={styles.body}>
           <h1 className={styles.header}>Interviewing at Kapwing</h1>
+          <div className={styles.content}>
+            <p className={styles.p}>
+              Thank you for taking the time to apply to{" "}
+              <a href="https://www.kapwing.com">Kapwing</a>. This interview
+              website contains the code that gets used in our engineering
+              interview process. We know it can be a big commitment to apply for
+              a new opportunity. We hope to honor your time and treat everyone
+              with respect throughout the entire process.
+            </p>
+            <p className={styles.p}>
+              To learn more about what it&apos;s like to work at Kapwing, please
+              visit our{" "}
+              <a href="https://www.kapwing.com/careers">careers site</a>.
+              We&apos;d also love for you to try out any of our video editing{" "}
+              <a href="https://www.kapwing.com/tools">tools</a>, and leave us
+              any feedback.
+            </p>
+          </div>
         </div>
       </main>
     </>
