8ace92c5acd7e643bd7d2ed1873256e73b769010
Pete Walker <108812366+petertimwalker@users.noreply.github.com>
2024-10-17 23:46:51 -0400

    add basic test for home page change

---
+++ src/pages/index.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Home from "./index";
+
+jest.mock("next/router", () => require("next-router-mock"));
+
+describe("Home", () => {
+  it("renders the title", () => {
+    render(<Home />);
+    expect(
+      screen.getByText("Kapwing Interview Project by Peter Walker")
+    ).toBeInTheDocument();
+  });
+});
