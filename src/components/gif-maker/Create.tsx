f95f1c2122efde4a787831ff1a7030ea9ce8dcb7
Pete Walker <108812366+petertimwalker@users.noreply.github.com>
2024-10-19 18:28:31 -0400

    add error message when gif call fails

--- src/components/gif-maker/Create.tsx
+++ src/components/gif-maker/Create.tsx
@@ -9,6 +9,7 @@ interface CreateProps {
 const CreateButton = ({ urls, intervalDuration }: CreateProps) => {
   const [loading, setLoading] = useState(false);
   const [gif, setGif] = useState("");
+  const [error, setError] = useState("");
 
   const create = () => {
     const body = {
@@ -29,13 +30,18 @@ const CreateButton = ({ urls, intervalDuration }: CreateProps) => {
       .then((res) => res.json())
       .then(
         (result) => {
-          const { gifUrl } = result;
+          const { gifUrl, error } = result;
+          setLoading(false);
+          if (error) {
+            setError(error);
+            return;
+          }
           // Append a timestamp to the GIF URL to prevent caching
           const cachedGifUrl = `${gifUrl}?t=${new Date().getTime()}`;
           setGif(cachedGifUrl);
-          setLoading(false);
         },
         (error) => {
+          setError("Internal server error");
           console.error("An error occurred:", error);
           setLoading(false);
         }
@@ -64,6 +70,14 @@ const CreateButton = ({ urls, intervalDuration }: CreateProps) => {
         <input type="button" value="Create GIF" onClick={create} />
       )}
       {renderFinalGif()}
+      {error && (
+        <div className={styles.errorContainer}>
+          <h2>Oops! Something went wrong while creating your GIF</h2>
+          <p className={styles.errorMessage}>{error}</p>
+          <br />
+          <p>We are sorry for the inconvenience. Please try again later</p>
+        </div>
+      )}
     </div>
   );
 };
