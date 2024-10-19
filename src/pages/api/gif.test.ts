6e0cda0b980804037ae74451e0dc2d09d5978609
Pete Walker <108812366+petertimwalker@users.noreply.github.com>
2024-10-19 14:05:40 -0400

    implement gif endpoint with test coverage

---
+++ src/pages/api/gif.test.ts
@@ -0,0 +1,90 @@
+import { createMocks } from "node-mocks-http";
+import handler from "./gif";
+import { NextApiRequest, NextApiResponse } from "next";
+
+jest.mock("@/helpers/files", () => ({
+  downloadFileFromUrl: jest.fn().mockResolvedValue("localPath1"),
+  getNameAndExtensionFromUrl: jest
+    .fn()
+    .mockReturnValue({ basename: "test", extension: ".jpg" }),
+  makeTempFilePathFromUrl: jest.fn().mockReturnValue("tempPath.gif"),
+  uploadFileFromLocalPath: jest
+    .fn()
+    .mockResolvedValue("https://aws.s3.mock.gif"),
+}));
+
+jest.mock("canvas", () => ({
+  createCanvas: jest.fn().mockReturnValue({
+    getContext: jest.fn().mockReturnValue({
+      drawImage: jest.fn(),
+    }),
+    toBuffer: jest.fn().mockReturnValue(Buffer.from("")),
+  }),
+  Image: class {
+    width = 250;
+    height = 250;
+    onload: () => void = () => {};
+    set src(_: string) {
+      this.onload();
+    }
+  },
+}));
+
+jest.mock("fs/promises", () => ({
+  unlink: jest.fn().mockResolvedValue(undefined),
+}));
+
+jest.mock("gif-encoder-2", () => {
+  return jest.fn().mockImplementation(() => ({
+    createReadStream: jest.fn().mockReturnValue({
+      pipe: jest.fn(),
+    }),
+    start: jest.fn(),
+    setDelay: jest.fn(),
+    addFrame: jest.fn(),
+    finish: jest.fn(),
+  }));
+});
+
+jest.mock("fs", () => ({
+  createWriteStream: jest.fn().mockReturnValue({
+    on: jest.fn((event, callback) => {
+      if (event === "close") {
+        callback();
+      }
+    }),
+  }),
+}));
+
+describe("GIF Endpoint", () => {
+  it("should call the endpoint and receive a response", async () => {
+    const { req, res } = createMocks<NextApiRequest, NextApiResponse>({
+      method: "POST",
+      url: "/api/gif",
+      body: { urls: ["http://example.com/image.png"] },
+    });
+
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(200);
+    expect(res._getJSONData()).toEqual({
+      gifUrl: "https://aws.s3.mock.gif",
+    });
+  });
+
+  it("should return a 500 status if the upload fails", async () => {
+    const { req, res } = createMocks<NextApiRequest, NextApiResponse>({
+      method: "POST",
+      url: "/api/gif",
+      body: { urls: ["http://example.com/image.png"] },
+    });
+
+    jest
+      .spyOn(require("@/helpers/files"), "uploadFileFromLocalPath")
+      .mockResolvedValueOnce("");
+
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(500);
+  });
+});
--- src/pages/api/gif.ts
+++ src/pages/api/gif.ts
@@ -1,6 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import sharp from "sharp";
-import GifEncoder from "gif-encoder";
+import {
+  downloadFileFromUrl,
+  getNameAndExtensionFromUrl,
+  makeTempFilePathFromUrl,
+  uploadFileFromLocalPath,
+} from "@/helpers/files";
+import { unlink } from "fs/promises";
+const GIFEncoder = require("gif-encoder-2");
+const { createCanvas, Image } = require("canvas");
+const { createWriteStream } = require("fs");
 
 type ResponseData = {};
 
@@ -8,14 +16,100 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  /*
-   *
-   * Your server side GIF maker implementation should start here
-   *
-   * One way to do this is using sharp and GifEncoder
-   *
-   */
-  res.status(200).json({ hello: "world!" });
+  const { body } = req;
+  const { urls } = body;
+
+  const files: string[] = await downloadFilesLocally();
+
+  // get the name of the first file to use as the name of the GIF
+  const { basename } = getNameAndExtensionFromUrl(urls[0]);
+  const gifFileName = `${basename}.gif`;
+  const localOutputPath = makeTempFilePathFromUrl(gifFileName);
+
+  await createGif("neuquant", localOutputPath, files);
+
+  const uploadedUrl = await uploadFileFromLocalPath(
+    localOutputPath,
+    gifFileName
+  );
+
+  if (uploadedUrl) {
+    await cleanupLocalFiles([...files, localOutputPath]);
+    return res.status(200).json({ gifUrl: uploadedUrl });
+  }
+
+  return res.status(500);
+
+  async function cleanupLocalFiles(filePaths: string[]) {
+    const unlinkPromises = filePaths.map((filePath) =>
+      unlink(filePath).then(
+        () => console.info(`File deleted: ${filePath}`),
+        (error) =>
+          console.error(
+            `Failed to delete file: ${filePath}, error: ${error.message}`
+          )
+      )
+    );
+    await Promise.allSettled(unlinkPromises);
+  }
+
+  async function downloadFilesLocally() {
+    const files: string[] = [];
+    for (const url of urls) {
+      const localInputUrl = await downloadFileFromUrl(url);
+      if (localInputUrl) {
+        files.push(localInputUrl);
+      }
+    }
+    return files;
+  }
+
+  async function createGif(
+    algorithm: string,
+    localOutputPath: string,
+    files: string[]
+  ) {
+    return new Promise<void>(async (resolve1) => {
+      // Create a new GIF file
+      // find the width and height of the image
+      const [width, height] = await new Promise<[number, number]>(
+        (resolve2) => {
+          const image = new Image();
+          image.onload = () => resolve2([image.width, image.height]);
+          image.src = files[0];
+        }
+      );
+
+      const writeStream = createWriteStream(localOutputPath);
+
+      // when stream closes GIF is created so resolve promise
+      writeStream.on("close", () => {
+        resolve1();
+      });
+      const encoder = new GIFEncoder(width, height, algorithm);
+      // pipe encoder's read stream to our write stream
+      encoder.createReadStream().pipe(writeStream);
+      encoder.start();
+      encoder.setDelay(200);
+
+      const canvas = createCanvas(width, height);
+      const ctx = canvas.getContext("2d");
+
+      // draw an image for each file and add frame to encoder
+      for (const file of files) {
+        await new Promise<void>((resolve3) => {
+          const image = new Image();
+          image.onload = () => {
+            ctx.drawImage(image, 0, 0);
+            encoder.addFrame(ctx);
+            resolve3();
+          };
+          image.src = file;
+        });
+      }
+      encoder.finish();
+    });
+  }
 }
 
 export const config = {
