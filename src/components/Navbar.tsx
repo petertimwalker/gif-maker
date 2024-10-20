cf1b0541b3c0be4f8fa8a37b09a32e50e5dcb2d9
Pete Walker <108812366+petertimwalker@users.noreply.github.com>
2024-10-19 20:48:31 -0400

    make nav bar colorful

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,39 +1,51 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./Navbar.module.scss";
 
 export default function Navbar() {
   const router = useRouter();
 
   return (
-    <div className={styles.container}>
-      <div className={styles.item}>
-        <Link
-          href="/"
-          className={router.pathname == "/" ? styles.activeLink : styles.link}
-        >
-          Home
-        </Link>
-      </div>
-      <div className={styles.item}>
-        <Link
-          href="/grayscale"
+    <div className="bg-white p-4 shadow-md">
+      <div className="flex space-x-4">
+        <div
           className={
-            router.pathname == "/grayscale" ? styles.activeLink : styles.link
+            router.pathname == "/"
+              ? "bg-teal-100 text-teal-700 p-2 rounded"
+              : "text-teal-700 p-2 rounded"
           }
         >
-          Grayscale
-        </Link>
-      </div>
-      <div className={styles.item}>
-        <Link
-          href="/gif-maker"
+          <Link href="/" className={router.pathname == "/" ? "font-bold" : ""}>
+            Home
+          </Link>
+        </div>
+        <div
           className={
-            router.pathname == "/gif-maker" ? styles.activeLink : styles.link
+            router.pathname == "/grayscale"
+              ? "bg-yellow-100 text-yellow-700 p-2 rounded"
+              : "text-yellow-700 p-2 rounded"
           }
         >
-          GIF Maker
-        </Link>
+          <Link
+            href="/grayscale"
+            className={router.pathname == "/grayscale" ? "font-bold" : ""}
+          >
+            Grayscale
+          </Link>
+        </div>
+        <div
+          className={
+            router.pathname == "/gif-maker"
+              ? "bg-red-100 text-red-700 p-2 rounded"
+              : "text-red-700 p-2 rounded"
+          }
+        >
+          <Link
+            href="/gif-maker"
+            className={router.pathname == "/gif-maker" ? "font-bold" : ""}
+          >
+            GIF Maker
+          </Link>
+        </div>
       </div>
     </div>
   );
